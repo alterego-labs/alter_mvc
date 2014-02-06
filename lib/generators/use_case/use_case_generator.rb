68c6d39d6cd2540065b284fb6800a5f22c870eb9
sergio1990 <sergeg1990@gmail.com>
2014-02-06 15:11:04 +0200

    -Added use case generator

---
+++ lib/generators/use_case/use_case_generator.rb
@@ -0,0 +1,13 @@
+require 'generators/alter_admin'
+
+module AlterMvc
+  module Generators
+    class UseCaseGenerator < Base
+
+      def add_use_case
+        template "use_case.rb", File.join('app/use_cases/', "#{file_name.singularize}_use_case.rb")
+      end
+
+    end
+  end
+end
