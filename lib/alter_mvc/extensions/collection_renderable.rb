6621625111d3a8e20bb95f0bdcf64e3b1459d087
sergio1990 <sergeg1990@gmail.com>
2014-05-30 15:26:23 +0300

    Added options to collection rendering

--- lib/alter_mvc/extensions/collection_renderable.rb
+++ lib/alter_mvc/extensions/collection_renderable.rb
@@ -1,8 +1,8 @@
 module AlterMvc::Extensions::CollectionRenderable
 
-  def rendering(as: nil, with: nil)
+  def rendering(as: nil, with: nil, options: {})
     fetch_collection.each.with_index.inject(ActiveSupport::SafeBuffer.new) do |output, (element,index)|
-      output << element.rendering(as: as, with: with, options: {index: index})
+      output << element.rendering(as: as, with: with, options: options.merge(index: index))
     end
   end
 
